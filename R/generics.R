af9c1616733a514c9b438d2d23d7d5c7eacbcdaa
Tim Triche, Jr <tim.triche@gmail.com>
2025-03-23 12:54:41 -0400

    add 'missingness' generic for easy stagewise updates

--- R/generics.R
+++ R/generics.R
@@ -116,3 +116,55 @@ if (!isGeneric("getUnmeth")) {
 setMethod("getUnmeth", "SummarizedExperiment", 
           function(object) (1 - getBeta(object)) * (2 ** getCN(object)))
 
+
+# missingness: 0-1 value for how many NAs are present in each column (default) or row of X
+if (!isGeneric("missingness")) {
+  setGeneric("missingness", 
+             function(X, MARGIN, i, ...) standardGeneric("missingness"))
+}
+
+# somewhat ironic default signature for 'missingness(X="ANY", MARGIN="missing", i="missing")
+setMethod("missingness", c(X="ANY", MARGIN="missing", i="missing"),
+          function(X, MARGIN, i, ...) missingness(X, MARGIN=2, i=1))
+
+# more specific signature: 'missingness(X="ANY, MARGIN=int, i="missing")
+setMethod("missingness", c(X="ANY", MARGIN="numeric", i="missing"),
+          function(X, MARGIN, i, ...) missingness(X, MARGIN=MARGIN, i=1))
+
+# avoid writing multiple methods for assay indexing 
+setClassUnion("num_or_char", c("numeric", "character"))
+
+# more specific signature: 'missingness(X="ANY", MARGIN="missing", i="num_or_char")
+setMethod("missingness", c(X="ANY", MARGIN="missing", i="num_or_char"),
+          function(X, MARGIN, i, ...) missingness(X, MARGIN=2, i=i))
+
+# fully specified method for a SummarizedExperiment object
+setMethod("missingness", c("SummarizedExperiment", "numeric", "num_or_char"),
+          function(X, MARGIN, i, ...) {
+            if (0L == dim(X)[MARGIN]) {
+              stop(paste0("Nothing to evaluate: dim(<", class(X), ">)[", MARGIN, "] == 0"))
+            }
+            if (0L == length(assays(X))) {
+              stop(paste0("Nothing to evaluate: length(assays(<", class(X), ">)) == 0"))
+            }
+            if (is.numeric(i) & length(assays(X)) < i) {
+              stop(paste0("Nothing to evaluate: length(assays(<", class(X), ">)) < ", i))
+            }
+            if (is.character(i) & !(i %in% assayNames(X))) {
+              stop(paste0("Nothing to evaluate: ", i, " not in assayNames(<", class(X), ">)"))
+            }
+            switch(MARGIN,
+                   rowSums(is.na(assay(X, i))) / ncol(X),
+                   colSums(is.na(assay(X, i))) / nrow(X))
+          })
+
+# fully specified method for any other type of matrix-like object
+setMethod("missingness", c("matrix", "numeric", "num_or_char"),
+          function(X, MARGIN, i, ...) {
+            if (0L == dim(X)[MARGIN]) {
+              stop(paste0("Nothing to evaluate: dim(<", class(X), ">)[", MARGIN, "] == 0"))
+            }
+            switch(MARGIN,
+                   rowSums(is.na(X)) / ncol(X),
+                   colSums(is.na(X)) / nrow(X))
+          })
