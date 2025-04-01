704bdd7eff97cca507368a4a6544c4f872a3082e
Tim Triche, Jr. <tim.triche@gmail.com>
2025-04-01 11:05:57 -0400

    add more informative messages to plotME

--- R/plotME.R
+++ R/plotME.R
@@ -40,9 +40,11 @@ plotME <- function(x, k=100, minmean=0.2, maxmean=0.8, ...) {
                   ...)
     if ("SNPs" %in% names(metadata(x))) { 
       message("Plotting SNPs...")
-      H2 <- try(plotSNPcalls(x, rotate=TRUE))
+      H2 <- try(suppressMessages(plotSNPcalls(x, rotate=TRUE), silent=TRUE))
       if (!inherits(H2, "try-error")) { 
         H1 + H2
+      } else { 
+        message("SNP calling failed, omitting SNP plot")
       }
     }
     H1
