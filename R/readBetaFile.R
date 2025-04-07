955845b551686c9f9f736b2e40f2b772754196d0
Tim Triche, Jr. <tim.triche@gmail.com>
2025-04-07 13:36:09 -0400

    autodetect hg19 .beta files (but do not autoannotate)

--- R/readBetaFile.R
+++ R/readBetaFile.R
@@ -6,7 +6,9 @@
 #'
 #' @return      a matrix of methylated (column 1) and total (column 2) reads
 #'
-#' @details     the output is nearly useless without the CpG dictionary. 
+#' @details     the output is nearly useless without the CpG dictionary. If the
+#'              size of the file is 56434896, we know g == "hg19", but the CpG
+#'              dictionary for hg19 is 125MB, so we don't have it in data(). 
 #' 
 #' @import      GenomicRanges
 #'
@@ -15,6 +17,10 @@
 readBetaFile <- function(x, CpG=NULL, g=NULL) { 
   N <- file.info(x)$size
   x <- matrix(readBin(x, "integer", N, size=1, signed=0), N/2, 2, byrow=TRUE)
+  if (nrow(x) == 28217448) {
+    message("This is an hg19 .beta file: 28217448 CpGs")
+    g <- "hg19"
+  }
   if (!is.null(CpG)) { 
     gr <- import(CpG)
     rownames(x) <- as.character(gr)
