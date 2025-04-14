44a5981879e0b0fa8e18daee0f371847960cc19d
Tim Triche, Jr. <tim.triche@gmail.com>
2025-04-14 13:20:02 -0400

    simplify impute.classKNN

--- R/impute.classKNN.R
+++ R/impute.classKNN.R
@@ -1,43 +1,45 @@
 #' impute missing sites via k-NN with user-defined classes for comparables
 #'
-#' @param X       a MethylationExperiment (or something with getBeta)
-#' @param INDEX   a factor representing the class of each observation
+#' @param x       a MethylationExperiment (or something with getBeta)
+#' @param col     a factor with the class of each column, or its name in colData
 #' @param ...     other arguments to pass along to impute::impute.knn
 #'
 #' @details       in principle, this can be combined with impute.GimmeCpG
 #'
-#' @return        x, but with a new assay Imputed that has been imputed.
+#' @return        x, but with a new assay named ImputedBeta 
 #' 
 #' @seealso       impute::impute.knn
 #'
 #' @importFrom    impute impute.knn
 #' 
 #' @export
-impute.classKNN <- function(X, INDEX, ...) { 
+impute.classKNN <- function(x, col, ...) { 
 
-  if (!is(X, "SummarizedExperiment") | !("Beta" %in% assayNames(X))) { 
+  if (!is(x, "SummarizedExperiment") | !("Beta" %in% assayNames(x))) { 
     stop("Could not find beta values to impute")
   }
-
-  tbl <- table(INDEX) 
-  k <- pmax(1, as.integer(tbl) - 1)
-  names(k) <- names(tbl) 
-  assay(X, "Imputed", withDim=FALSE) <- .imputeByClass(X, INDEX, k)
-  return(X) 
-
-}
-
-
-# helper fn
-.imputeByClass <- function(X, INDEX, k, ...) { 
-
-  B <- getBeta(X)
-  for (i in names(k)) { 
-    nn <- k[i]
-    j <- which(INDEX == i)
-    message("Imputing missing values for class ", i, "...") 
-    B[, j] <- fexpit(impute.knn(flogit(B[, j]), k=nn, ...)$data)
+  if (length(col) != ncol(x)) {
+    if (col %in% names(colData(x))) { 
+      col <- colData(x)[, col]
+    } else {
+      stop("col does not match ncol(x)")
+    }
   }
-  return(B)
+
+  tbl <- table(col)
+  assay(x, "ImputedBeta") <- getBeta(x)
+  k <- pmax(1, (as.integer(tbl) - 1))
+  names(k) <- names(tbl) 
+
+  # replace with bplapply?
+  for (i in names(k)) {
+    nn <- k[i]
+    j <- which(col == i)
+    message("Imputing missing values for class ", i, "...") 
+    assay(x[, j], "ImputedBeta") <- 
+      fexpit(impute.knn(flogit(assay(x[, j], "ImputedBeta")), k=nn, ...)$data)
+  }
+
+  return(x)
 
 }
