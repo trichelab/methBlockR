71672346814f142d5a518557caa4851f319532ca
Tim Triche, Jr. <tim.triche@gmail.com>
2025-04-14 14:00:14 -0400

    add a cap for maxNA so as not to over-impute

--- R/impute.classKNN.R
+++ R/impute.classKNN.R
@@ -2,9 +2,11 @@
 #'
 #' @param x       a MethylationExperiment (or something with getBeta)
 #' @param col     a factor with the class of each column, or its name in colData
+#' @param maxNA   maximum row NA fraction (0 to 1) allowed within a class (0.33)
 #' @param ...     other arguments to pass along to impute::impute.knn
 #'
-#' @details       in principle, this can be combined with impute.GimmeCpG
+#' @details       in principle, this could be combined with impute.GimmeCpG
+#'                in practice, can iterate this with coarsening 'col' factors
 #'
 #' @return        x, but with a new assay named ImputedBeta 
 #' 
@@ -13,7 +15,7 @@
 #' @importFrom    impute impute.knn
 #' 
 #' @export
-impute.classKNN <- function(x, col, ...) { 
+impute.classKNN <- function(x, col, maxNA=0.33, ...) { 
 
   if (!is(x, "SummarizedExperiment") | !("Beta" %in% assayNames(x))) { 
     stop("Could not find beta values to impute")
@@ -28,16 +30,17 @@ impute.classKNN <- function(x, col, ...) {
 
   tbl <- table(col)
   assay(x, "ImputedBeta") <- getBeta(x)
-  k <- pmax(1, (as.integer(tbl) - 1))
+  k <- pmin(10, pmax(1, (as.integer(tbl) - 1)))
   names(k) <- names(tbl) 
 
   # replace with bplapply?
   for (i in names(k)) {
     nn <- k[i]
     j <- which(col == i)
+    rr <- which(missingness(x[, j], 1, "ImputedBeta") <= maxNA)
     message("Imputing missing values for class ", i, "...") 
-    assay(x[, j], "ImputedBeta") <- 
-      fexpit(impute.knn(flogit(assay(x[, j], "ImputedBeta")), k=nn, ...)$data)
+    assay(x[rr, j], "ImputedBeta") <- 
+      fexpit(impute.knn(flogit(assay(x[rr, j], "ImputedBeta")), k=nn, ...)$data)
   }
 
   return(x)
