08ed8f85a71ba20f7ca4c7bf4c2bdec0845d27a6
Tim Triche, Jr. <tim.triche@gmail.com>
2025-04-16 09:40:12 -0400

    enhancements to blockDownsample

--- R/blockDownsample.R
+++ R/blockDownsample.R
@@ -1,15 +1,16 @@
 #' block-randomized sampling to ensure somewhat representative comparisons 
 #'
-#' @param x       a MethylationExperiment or comparable object
+#' @param x       a MethylationExperiment or comparable rectangular object
 #' @param col     a factor for each column, or its name in colData
-#' @param SNPcall recall SNPs if present? (TRUE)
+#' @param maxN    maximum size per-subset? (Inf; use the smallest subset size)
+#' @param SNPcall recall SNPs if present? (FALSE)
 #'
 #' @details       use set.seed beforehand if you want reproducibility.
 #'
 #' @return        a subset of x, downsampled to match the smallest N(col)
 #' 
 #' @export
-blockDownsample <- function(x, col, SNPcall=TRUE) {
+blockDownsample <- function(x, col, maxN=Inf, SNPcall=FALSE) {
 
   if (length(col) != ncol(x)) {
     if (col %in% names(colData(x))) col <- colData(x)[, col]
@@ -18,23 +19,26 @@ blockDownsample <- function(x, col, SNPcall=TRUE) {
 
   ss <- levels(factor(col))
   names(ss) <- ss
-  n <- min(table(col))
+  n <- min(maxN, min(table(col)))
   idx <- sort(do.call(c, lapply(ss, function(s) sample(which(col == s), n))))
   res <- x[, idx]
-  mdn <- names(metadata(res))
-  tbl <- table(mdn) 
-  if ("SNPs" %in% names(tbl)) { 
-    if (all(colnames(res) %in% colnames(metadata(res)$SNPs) == ncol(x))) { 
-      metadata(res)[["SNPs"]] <- metadata(res)[["SNPs"]][, colnames(res)]
-      if ("SNPcalls" %in% names(tbl)) {
-        metadata(res)[which(mdn == "SNPcalls")] <- NULL
-        if (SNPcall) {
-          message("Recalling SNPs for the merged dataset.") 
-          metadata(res)[["SNPcalls"]] <- SNPcalls(res)
+
+  if (is(x, "SummarizedExperiment")) {
+    mdn <- names(metadata(res))
+    tbl <- table(mdn) 
+    if ("SNPs" %in% names(tbl)) { 
+      if (all(colnames(res) %in% colnames(metadata(res)$SNPs) == ncol(x))) { 
+        metadata(res)[["SNPs"]] <- metadata(res)[["SNPs"]][, colnames(res)]
+        if ("SNPcalls" %in% names(tbl)) {
+          metadata(res)[which(mdn == "SNPcalls")] <- NULL
+          if (SNPcall) {
+            message("Recalling SNPs for the merged dataset.") 
+            metadata(res)[["SNPcalls"]] <- SNPcalls(res)
+          }
         }
+      } else {  
+        message("Missing columns in metadata(<",class(res),">)$SNPs; skipped")
       }
-    } else {  
-      message("Missing columns in metadata(<",class(res),">)$SNPs; skipped")
     }
   }
 
