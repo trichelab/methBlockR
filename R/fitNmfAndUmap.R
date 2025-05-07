7de97aa614e1bc423cc89c22c5482ac9af903f58
Tim Triche, Jr. <tim.triche@gmail.com>
2025-05-07 12:20:42 -0400

    update fitNmfAndUmap to hopefully squash some bugs

--- R/fitNmfAndUmap.R
+++ R/fitNmfAndUmap.R
@@ -1,7 +1,7 @@
 #' performs NMF on the beta values, stuffs the factors into colData, runs UMAP
 #' 
 #' @param x   a MethBlockExperiment or similar thing-with-Beta-values
-#' @param k   number of factors to find (30)
+#' @param k   number of factors to find (30) (FIXME: use ARD instead)
 #' @param ... other arguments to pass to RcppML::nmf
 #'
 #' @return    SCE, with new reducedDim()s, colData, nmf_fit & umap_fit metadata
@@ -11,19 +11,20 @@
 #' @import    SingleCellExperiment
 #' @import    RSpectra
 #' @import    RcppML
+#' @import    Matrix
 #' @import    uwot
 #'
 #' @export
 #'
 fitNmfAndUmap <- function(SCE, k=30, ...) {
 
-  .NA2ZERO <- function(x) ifelse(is.na(x), 0, x) # mask=NA no worky
   if (!is(SCE, "SingleCellExperiment")) { 
     SCE <- as(SCE, "SingleCellExperiment") # if a SummarizedExperiment
   }
+  k <- min(k, ncol(SCE))
   message("Fitting NMF with ", k, " components... ", appendLF = FALSE)
-  nmf_fit <- nmf(.NA2ZERO(squeeze(getBeta(SCE))), mask = "zeros", k = k, ...,
-                 tol = 1e-3, L1 = 0.01, maxit = 1000, seed = 1234)
+  nmf_fit <- nmf(as(.NA2ZERO(squeeze(getBeta(SCE))), "dMatrix"),
+                 mask="zeros", k=k, L1=0.01, ...)
   message("done.")
   reducedDim(SCE, "NMF") <- as(t(nmf_fit@h), "matrix") # so UMAP doesn't croak
   cols <- colnames(reducedDim(SCE, "NMF"))
@@ -33,10 +34,17 @@ fitNmfAndUmap <- function(SCE, k=30, ...) {
   message("done.")
   metadata(SCE)$nmf_fit <- nmf_fit
   message("Fitting UMAP on NMF hat matrix... ", appendLF = FALSE)
-  umap_fit <- umap(reducedDim(SCE, "NMF"), metric="cosine", ret_model=TRUE)
+  umap_fit <- umap(reducedDim(SCE, "NMF"), n_neighbors=min(ncol(SCE), 15),
+                   metric="cosine", ret_model=TRUE)
   reducedDim(SCE, "UMAP") <- umap_fit$embedding
   metadata(SCE)$umap_fit <- umap_fit
   message("done.")
   return(SCE)
 
+}
+
+
+# helper fn
+.NA2ZERO <- function(x) {
+  ifelse(is.na(x), 0, x)
 }
